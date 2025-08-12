a6e371a9ec7124b1e5bdaa922a366703a62f67c4
prodyut <prodyut@kloudgurdian.com>
2025-08-12 12:56:11 +0530

    VM deploy in GCP

---
+++ main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("gcp-key.json")
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone
+}
+
+variable "project_id" {}
+variable "region" {}
+variable "zone" {}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "terraform-vm"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+
+    access_config {}
+  }
+}
