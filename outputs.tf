1f2a439d6d0371b1a5109224ef7c2ca004f205e3
prodyut <prodyut@kloudgurdian.com>
2025-08-12 16:04:47 +0530

    asda

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,7 @@
 output "instance_name" {
-  description = "The name of the VM"
-  value       = google_compute_instance.vm.name
+  value       = google_compute_instance.vm_instance.name
 }
 
 output "instance_ip" {
-  description = "The external IP of the VM"
-  value       = google_compute_instance.vm.network_interface[0].access_config[0].nat_ip
+  value       = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
 }
